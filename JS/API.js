3f66d2dd085364ecc98db31051ab378b4787bb99
bagswicaksana <bwicaksna29@gmail.com>
2023-10-05 16:28:41 +0700

    edit API

--- JS/API.js
+++ JS/API.js
@@ -1,16 +1,16 @@
-// const API_BASE_URL = 'https://be-balikpapan-23-'
+const API_BASE_URL = 'http://localhost:3000'
 
 const fetchReceiptById = async () => {
     const id_receipt = document.getElementById('lacak').value;
     console.log(id_receipt);
     try {
-        const response = await fetch(`${API_BASE_URL}/tracking/${id_receipt}`);
+        const response = await fetch(`${API_BASE_URL}/status/${id_receipt}`);
         const tracking = await response.json();
         console.log(tracking);
 
         const receiptDetails = document.getElementById('modal-lacak');
         receiptDetails.innerHTML = `
-        Receipt Number  : ${receipt.dat}
+        Receipt Number  : ${tracking.no_resi}
         Origin          :
         Destination     :
         Sender          :
