d149b35732cf7bdfc2400a3f9dc48d39b53f110b
marsyandarzt <marsyandarz@gmail.com>
2023-10-08 02:24:44 +0700

    edit API n index js for cek resi dan cek ongkir

--- JS/API.js
+++ JS/API.js
@@ -1,18 +1,6 @@
 const API_BASE_URL = 'http://localhost:3000'
 
-
-async function fetchBookById() {
-    const bookId = document.getElementById('book-id').value;
-    try {
-        const response = await fetch(`${API_BASE_URL}/books/${bookId}`);
-        const book = await response.json();
-        const bookDetails = document.getElementById('book-details');
-        bookDetails.innerHTML = `Title: ${book.title}<br>Author: ${book.author}`;
-    } catch (error) {
-        console.error('Error fetching book:', error);
-    }
-  }
-
+// C E K   R E S I
 const fetchReceiptById = async () => {
     const noResi = document.getElementById('lacak').value;
     try {
@@ -25,36 +13,54 @@ const fetchReceiptById = async () => {
         detailResi.innerHTML = `
         <h1>Hasil Pencarian</h1>
         <div>
-            <input type="text" placeholder="NO RESI" value="${tracking.no_resi}">
-            <input type="text" placeholder="ASAL" value="${tracking.asal}">
-            <input type="text" placeholder="TUJUAN" value="${tracking.tujuan}">
-            <input type="text" placeholder="NAMA PENGIRIM" value="${tracking.nama_pengirim}">
-            <input type="text" placeholder="NAMA PENERIMA" value="${tracking.nama_penerima}">
-            <input type="text" placeholder="STATUS"value="${tracking.status}">
+        <label for="no_resi">No resi:</label>
+        <input type="text" id="no_resi placeholder="NO RESI" value="${tracking.no_resi}">
+
+        <label for="asal">Asal:</label>
+        <input type="text" id="asal" placeholder="ASAL" value="${tracking.asal}">
+
+        <label for="tujuan">Tujuan:</label>
+        <input type="text" id="tujuan" placeholder="TUJUAN" value="${tracking.tujuan}">
+
+        <label for="nama_pengirim">Nama Pengirim:</label>
+        <input type="text" id="nama_pengirim placeholder="NAMA PENGIRIM" value="${tracking.nama_pengirim}">
+
+        <label for="nama_penerima">Nama Penerima:</label>
+        <input type="text" id="nama_penerima placeholder="NAMA PENERIMA" value="${tracking.nama_penerima}">
+
+        <label for="status">Status:</label>
+        <input type="text" id="status" placeholder="STATUS"value="${tracking.status}">
         </div>
-        <button class="close-button-lacak">Kembali</button>
+        <button onclick="closeLacak()" class="close-button-lacak">Kembali</button>
         `
     } catch (error) {
         console.error(error);
     }
 }
 
-const fetchPricing = async () => {
-    const pricing = document.getElementById('myPopup').value;
-    console.log(pricing);
+// C E K   O N G K I R
+const fetchOngkir = async () => {
+    const asal = document.getElementById('opsi-asal').value;
+    const tujuan = document.getElementById('opsi-tujuan').value;
+    const berat = document.getElementById('berat-paket').value;
     try {
-        const response = await fetch(`${API_BASE_URL}/price/${pricing}`);
+        const response = await fetch(`${API_BASE_URL}/price?origin=${asal}&destination=${tujuan}&weight=${berat}`);
         const tarif = await response.json();
-        const detailOngkir = document.getElementById('myPopup');
+        const detailOngkir = document.getElementById('modal-ongkir');
         
         detailOngkir.innerHTML = `
         <h1>CEK</h1>
-        <div id="hasil-pengiriman">
-            <input type="text"  placeholder="ASAL" value="${tarif.origin}">
-            <input type="text"  placeholder="TUJUAN" value="${tarif.destination}">
-            <input type="text"  placeholder="BERAT" value="${tarif.price}">
-     
+        <div>
+            <label for="origin">Asal:</label>
+            <input type="text" id="origin" placeholder="ASAL" value="${tarif.origin}">
+
+            <label for="destination">Tujuan:</label>
+            <input type="text" id="destination" placeholder="TUJUAN" value="${tarif.destination}">
+
+            <label for="price">Estimasi ongkir:</label>
+            <input type="text" id="price" placeholder="HARGA" value="${tarif.price}">
         </div>
+        <button onclick="closeOngkir()" class="close-button-ongkir">Kembali</button>
         `
     } catch (error) {
         console.error(error);
@@ -62,20 +68,11 @@ const fetchPricing = async () => {
 }
 
 
-// const fetchMessage() {
-//     try {
-//         const response = await fetch(`${API_BASE_URL}/message`);
-//         const data = await response.json();
-//         const startDisplay = document.getElementById('container-message');
-//         startDisplay.innerHTML = '';
-//     }
-// }
-
 const createMessage = async () => {
-    const name = document.getElementById('contactUs-name').value;
+    const nama = document.getElementById('contactUs-name').value;
     const email = document.getElementById('contactUs-email').value;
-    const subject = document.getElementById('contactUs-subject').value;
-    const theMessage = document.getElementById('contactUs-theMessage').value;
+    const subjek = document.getElementById('contactUs-subject').value;
+    const pesan = document.getElementById('contactUs-theMessage').value;
 
     try {
         const response = await fetch(`${API_BASE_URL}/message`, {
@@ -83,12 +80,10 @@ const createMessage = async () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({name, email, subject, theMessage})
+            body: JSON.stringify({nama, email, subjek, pesan})
         });
         const data = await response.json();
     } catch (error) {
         console.error('Error sending message:', error);
-    } finally {
-        fetchMessage(); //Refresh the display
-    }
+    } 
 }
--- index.js
+++ index.js
@@ -6,122 +6,42 @@ hamberger.addEventListener("click", function () {
     menuNav.classList.toggle("active")
 })
 
-// check lokasi awal
-const menuOpsi = document.querySelector(".select-menu"),
-    selectButton = menuOpsi.querySelector(".select-button"),
-    opsi = menuOpsi.querySelectorAll(".provinsi"),
-    buttonText = menuOpsi.querySelector(".button-text");
 
-selectButton.addEventListener("click", () => menuOpsi.classList.toggle("active"));
-
-opsi.forEach(provinsi => {
-    provinsi.addEventListener("click", () => {
-        let pilihan = provinsi.querySelector(".text-provinsi").innerText;
-        buttonText.innerText = pilihan;
-        menuOpsi.classList.remove("active");
-    });
-});
-
-// check lokasi tujuan
-const menuOpsiTujuan = document.querySelector(".select-menu-tujuan"),
-    selectButtonTujuan = menuOpsiTujuan.querySelector(".select-button-tujuan"),
-    opsiTujuan = menuOpsiTujuan.querySelectorAll(".provinsi-tujuan"),
-    buttonTextTujuan = menuOpsiTujuan.querySelector(".button-text-tujuan");
-
-selectButtonTujuan.addEventListener("click", () => menuOpsiTujuan.classList.toggle("active"));
-
-opsiTujuan.forEach(provinsiTujuan => {
-    provinsiTujuan.addEventListener("click", () => {
-        let pilihanTujuan = provinsiTujuan.querySelector(".text-provinsi-tujuan").innerText;
-        buttonTextTujuan.innerText = pilihanTujuan;
-        menuOpsiTujuan.classList.remove("active");
-    });
-});
-
-
-
-// cek harga
-const lokasiAwalElement = document.getElementById("lokasi-awal");
-const lokasiTujuanElement = document.getElementById("lokasi-tujuan");
-const beratElement = document.getElementById("berat");
-const hargaElement = document.getElementById("harga");
-const submitButton = document.querySelector(".button-submit");
-
-submitButton.addEventListener("click", function () {
-
-    const hasilPengiriman = document.getElementById("hasil-pengiriman");
-    const lokasiAwal = document.querySelector(".select-button .button-text").textContent;
-    const lokasiTujuan = document.querySelector(".select-button-tujuan .button-text-tujuan").textContent;
-    const beratInput = document.getElementById("berat");
-    const berat = parseFloat(beratInput.value);
-
-    // validasi jika user tidak menginput lokasi
-    if (lokasiAwal === "Pilih Lokasi Awal" || lokasiTujuan === "Pilih Lokasi Tujuan") {
-        alert("Harap pilih kedua lokasi awal dan tujuan pengiriman.");
-        return;
-    }
-    // Validasi apakah berat adalah angka yang valid
-    if (isNaN(berat) || berat <= 0) {
-        alert("Berat harus berupa angka yang valid dan lebih dari 0.");
-        return;
-    } // Menghentikan eksekusi jika berat tidak valid
-
-    // Menambahkan "kg" di dalam berat saat menampilkan nilai berat
-    document.getElementById("berat-hasil").textContent = berat + " kg";
-
-    const hargaPerKg = 5000; // Harga per kilogram
-    
- 
-
-    // Hitung total harga berdasarkan berat
-    const totalHarga = berat * hargaPerKg;
-
-    // Set nilai elemen-elemen HTML sesuai dengan inputan pengguna
-    lokasiAwalElement.textContent = lokasiAwal;
-    lokasiTujuanElement.textContent = lokasiTujuan;
-    beratElement.textContent = berat + " kg"; // Menambahkan "kg" di dalam berat
-    hargaElement.textContent = "Rp. " + totalHarga.toLocaleString(); // Mengubah menjadi rupiah
-
-    // Tampilkan hasil pengiriman
-    hasilPengiriman.style.display = "block"; // Mengubah properti display menjadi "block" untuk menampilkan hasil pengiriman
-});
-
-// pop up
-function showPopup() {
-    const lokasiAwal = document.querySelector(".select-button .button-text").textContent;
-    const lokasiTujuan = document.querySelector(".select-button-tujuan .button-text-tujuan").textContent;
-    const berat = parseFloat(document.getElementById("berat").value);
-
-    // validasi jika user tidak menginput lokasi
-    if (lokasiAwal === "Pilih Lokasi Awal" || lokasiTujuan === "Pilih Lokasi Tujuan") {
-        return;
-    }else if (isNaN(berat) || berat <= 0) {
-        return;
-    }
-    var popup = document.getElementById("myPopup");
-    popup.style.display = "block";
-}
-
-function closePopup() {
-    var popup = document.getElementById("myPopup");
-    popup.style.display = "none";
-}
-
-
-// tracking
-// Pop up lacak resi
+// LACAK PAKETMU
+// Untuk nampilin popup hasil pencarian resi
 const modal = document.querySelector("#modal-lacak");
 const openModal = document.querySelector(".open-button-lacak");
-const closeModal = document.querySelector(".close-button-lacak");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
 });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
+function closeLacak() {
+    const popupLacak = document.getElementById("modal-lacak");
+    popupLacak.style.display = "none";
+};
 
+
+// CEK ESTIMASI ONGKIRMU
+//Untuk nampilin popup hasil estimasi harga
+const ongkir = document.querySelector("#modal-ongkir");
+const openOngkir = document.querySelector(".open-button-ongkir");
+
+openOngkir.addEventListener("click", () => {
+    ongkir.showModal();
 });
+
+function closeOngkir() {
+    const popupOngkir = document.getElementById("modal-ongkir");
+    popupOngkir.style.display = "none";
+};
+
+
+// CONTACT US
+function createMessage() {
+    alert("Pesan Anda sudah terkirim.");
+}
+
 // carosel
 const carouselItems = document.querySelectorAll('.content-kanan');
 let currentIndex = 0;
